fa6e1c2be2c55a24c47872177cead049614a6dc9
nhn25 <nehuen.inos25@gmail.com>
2023-03-07 11:03:19 -0300

    limpieza

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import "assets/scss/now-ui-kit.scss?v=1.5.0";
 import "assets/demo/demo.css?v=1.5.0";
 import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
 // pages for this kit
-import Index from "views/Index.js";
 import Pdf from "components/Funcionales/PdfView.js";
-import NucleoIcons from "views/NucleoIcons.js";
 import LoginPage from "views/examples/LoginPage.js";
 import LandingPage from "views/examples/LandingPage.js";
 import SignUpPage from "./views/examples/SignUp.js";
@@ -27,11 +25,6 @@ root.render(
   <BrowserRouter>
     <Switch>
       <Switch>
-        <Route path="/index" render={(props) => <Index {...props} />} />
-        <Route
-          path="/nucleo-icons"
-          render={(props) => <NucleoIcons {...props} />}
-        />
         <Route
           path="/landing-page"
           render={(props) => <LandingPage {...props} />}
